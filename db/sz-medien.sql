28c04f2881edf22939544e193af5b3d16c12f0d8
Maximilian.goeckler <maximilian.goeckler@gmail.com>
2018-11-19 17:13:29 +0100

    db sql

--- db/sz-medien.sql
+++ db/sz-medien.sql
@@ -1,8 +1,9 @@
 -- phpMyAdmin SQL Dump
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
+--
 -- Host: 127.0.0.1
--- Erstellungszeit: 17. Nov 2018 um 10:57
+-- Erstellungszeit: 19. Nov 2018 um 17:12
 -- Server-Version: 10.1.35-MariaDB
 -- PHP-Version: 7.2.9
 
@@ -12,31 +13,30 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-
+--
 -- Datenbank: `sz-medien`
-
-
--- CREATE DATABASE IF NOT EXISTS sz-medien;
+--
 
 -- --------------------------------------------------------
 
-
+--
 -- Tabellenstruktur für Tabelle `bilder`
-
+--
 
 CREATE TABLE `bilder` (
   `id` int(11) NOT NULL,
   `link` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+--
 -- Daten für Tabelle `bilder`
-
+--
 
 INSERT INTO `bilder` (`id`, `link`) VALUES
 (1, 'images/cam_pics/pic_osmo1.jpg'),
@@ -46,9 +46,9 @@ INSERT INTO `bilder` (`id`, `link`) VALUES
 
 -- --------------------------------------------------------
 
-
+--
 -- Tabellenstruktur für Tabelle `cameras`
-
+--
 
 CREATE TABLE `cameras` (
   `id` int(11) NOT NULL,
@@ -67,9 +67,9 @@ CREATE TABLE `cameras` (
   `videolink` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+--
 -- Daten für Tabelle `cameras`
-
+--
 
 INSERT INTO `cameras` (`id`, `name`, `beschreibung`, `marke`, `akkulaufzeit`, `Kameratyp`, `Auflösung`, `Verschlusszeiten`, `ISO`, `Gewicht`, `Größe`, `Monitor`, `bildlink`, `videolink`) VALUES
 (1, 'DJI Osmo', 'Test Beschreibung', 'DJI', 0, 0, 0, 0, 0, 0, 0, 0, 'images/main-bild/test1.jpg', 'images/videos/1/test1.mp4'),
@@ -82,28 +82,50 @@ INSERT INTO `cameras` (`id`, `name`, `beschreibung`, `marke`, `akkulaufzeit`, `K
 
 -- --------------------------------------------------------
 
+--
+-- Tabellenstruktur für Tabelle `group`
+--
 
+CREATE TABLE `group` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `group`
+--
+
+INSERT INTO `group` (`id`, `name`) VALUES
+(1, 'Schüler'),
+(2, 'Lehrer'),
+(3, 'Andere');
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `user`
-
+--
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL,
+  `gruppe` varchar(30) NOT NULL,
   `pw` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+--
 -- Daten für Tabelle `user`
+--
 
-
-INSERT INTO `user` (`id`, `name`, `pw`) VALUES
-(1, 'Max', '2ec142711dc3799280b5c8747c8020a1fce3d172ab760256ffd74032c4c17d11');
+INSERT INTO `user` (`id`, `name`, `gruppe`, `pw`) VALUES
+(1, 'Max', 'SchÃ¼ler', '2ec142711dc3799280b5c8747c8020a1fce3d172ab760256ffd74032c4c17d11'),
+(6, 'Josef', 'Lehrer', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
 
 -- --------------------------------------------------------
 
-
+--
 -- Tabellenstruktur für Tabelle `zusatz`
-
+--
 
 CREATE TABLE `zusatz` (
   `camera_id` int(11) NOT NULL,
@@ -111,63 +133,63 @@ CREATE TABLE `zusatz` (
   `Beschreibung` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+--
 -- Indizes der exportierten Tabellen
+--
 
-
-
+--
 -- Indizes für die Tabelle `bilder`
-
+--
 ALTER TABLE `bilder`
   ADD KEY `id` (`id`);
 
-
+--
 -- Indizes für die Tabelle `cameras`
-
+--
 ALTER TABLE `cameras`
   ADD PRIMARY KEY (`id`);
 
-
+--
 -- Indizes für die Tabelle `user`
-
+--
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
-
+--
 -- Indizes für die Tabelle `zusatz`
-
+--
 ALTER TABLE `zusatz`
   ADD KEY `fk_zusatz_cameras` (`camera_id`);
 
-
+--
 -- AUTO_INCREMENT für exportierte Tabellen
+--
 
-
-
+--
 -- AUTO_INCREMENT für Tabelle `cameras`
-
+--
 ALTER TABLE `cameras`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
-
+--
 -- AUTO_INCREMENT für Tabelle `user`
-
+--
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
-
+--
 -- Constraints der exportierten Tabellen
+--
 
-
-
+--
 -- Constraints der Tabelle `bilder`
-
+--
 ALTER TABLE `bilder`
   ADD CONSTRAINT `bilder_ibfk_1` FOREIGN KEY (`id`) REFERENCES `cameras` (`id`);
 
-
+--
 -- Constraints der Tabelle `zusatz`
-
+--
 ALTER TABLE `zusatz`
   ADD CONSTRAINT `fk_zusatz_cameras` FOREIGN KEY (`camera_id`) REFERENCES `cameras` (`id`);
 COMMIT;
